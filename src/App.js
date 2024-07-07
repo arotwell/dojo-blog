c5696277cf2324d28115c99abafbc05f046001f8
Andrew <gft3fr@virginia.edu>
2024-07-07 04:16:59 -0400

    create navbar and home components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 const title = "Welcome to the new blog"
 const likes = 50;
-
+//const person = { name: "yoshi", age: 30};
+const link = "http://www.google.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>
-          { title }
-        </h1>
-        <p>
-          Liked { likes } times
-        </p>
+        <Home />
       </div>
     </div>
   );
