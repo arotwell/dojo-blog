95b6403878125a201c4d70f5fedcc28ab8f84827
Andrew <gft3fr@virginia.edu>
2024-07-09 00:49:01 -0400

    Added additional bloglist to show only mario's blogs

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web Dev Top Tool Tips', body: 'lorem ipsum...', author: 'luigi', id: 3 },
+        { title: 'Web Dev Top Tool Tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
     ]);
 
     
     return ( 
         <div className="home">
             <BlogList blogs={ blogs } title="All Blogs!"/>
+            <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title="Mario's Blogs!"/>
         </div>
      );
 }
