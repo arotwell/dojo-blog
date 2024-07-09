3af9f6f2253ec19e469f8a5d8c7067bc804be81d
Andrew <gft3fr@virginia.edu>
2024-07-09 01:41:55 -0400

    created json server for makeshift endpoint and added use effect to update the blogs when the page is rendered

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web Dev Top Tool Tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-    ]);
+    const [blogs, setBlogs] = useState([]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }    
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+            });
+    }, []);
     
     return ( 
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete }/>
+            <BlogList blogs={ blogs } title="All Blogs!"/>
         </div>
      );
 }
