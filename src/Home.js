4ff406375953c2b7c13ffccf3cc1ce6a85c546a6
Andrew <gft3fr@virginia.edu>
2024-07-11 01:46:35 -0400

    added error handling for fetching api endpoint

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,36 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
-
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+       setTimeout(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok){
+                    throw Error('Could not fetch data for that resource');
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
-            });
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+       }, 1000)
     }, []);
     
     return ( 
         <div className="home">
+            
             <BlogList blogs={ blogs } title="All Blogs!"/>
+            {isPending  && <div>Loading...</div>}
+            { error && <div>{ error }</div>}
         </div>
      );
 }
