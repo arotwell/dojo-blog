fadc5f2664455382242c6197ab8394270a9a16e1
Andrew <gft3fr@virginia.edu>
2024-07-15 23:40:37 -0400

    added abort for when fetch fails

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,9 @@ const useFetch = (url) => {
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
-        setTimeout(() => {
-         fetch(url)
+        const abortCont = new AbortController();
+        
+         fetch(url, { signal: abortCont.signal })
              .then(res => {
                  if (!res.ok){
                      throw Error('Could not fetch data for that resource');
@@ -20,10 +21,17 @@ const useFetch = (url) => {
                  setError(null);
              })
              .catch(err => {
-                 setError(err.message);
-                 setIsPending(false);
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                }
+                else {
+                    setError(err.message);
+                    setIsPending(false);
+                }
              })
-        }, 1000)
+        
+             
+        return () => abortCont.abort();
      }, [url]);
 
      return { data, isPending, error };
